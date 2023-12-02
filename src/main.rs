49498f6e20a2191d0e599c9cb428c2f621ef0eee
waverew <waverew@gmail.com>
2023-12-02 03:00:48 +0300

    bot sends hello world

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
-mod key;
-fn main() {
-    println!("Hello, world!");
+use teloxide::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+    log::info!("Starting throw dice bot...");
+
+    let bot = Bot::from_env();
+
+    /*teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        bot.send_dice(msg.chat.id).await?;
+        Ok(())
+    })*/
+    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        bot.send_message(msg.chat.id, "hello world".to_string()).await?;
+        Ok(())
+    })
+    .await;
 }
