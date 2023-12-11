c601031590ff9eeff817176446823abeb2fc9ffa
waverew <waverew@gmail.com>
2023-12-12 02:57:09 +0300

    added editin adn adding uchastki

--- src/zone.rs
+++ src/zone.rs
@@ -1,9 +1,11 @@
+#[derive(PartialEq, Copy, Clone)]
 pub struct Uchastok<'a> {
     zone: Zone<'a>,
     pub name: &'a str,
     description: &'a str
 }
 
+#[derive(PartialEq, Copy, Clone)]
 pub struct Zone<'a> {
     name: &'a str,
     observables: &'a str,
@@ -11,21 +13,38 @@ pub struct Zone<'a> {
     commentary: &'a str
 }
 
-const y: Zone = Zone{
+const Y: Zone = Zone{
     name: "лес",
     observables: "нужно удобрить",
     infrastructure: "трудно подъехать",
     commentary: "все плохо"
 };
 
-const x: Uchastok = Uchastok {
-    zone: y,
+const X: Uchastok = Uchastok {
+    zone: Y,
     name: "боброво",
     description: "небольшой поселок"
 };
 
 pub fn get_uchastki<'b>() -> Vec<Uchastok<'b>> {
     let mut uchastki = vec![];
-    uchastki.push(x);
+    uchastki.push(X);
     uchastki
+}
+
+pub fn add_uchastki(uch: Uchastok) -> Uchastok {
+    let mut uchastki = vec![];
+    uchastki.push(uch);
+    uch
+}
+pub fn edit_uchastok<'a>(uch: Uchastok<'a>, new_uchastok: Uchastok<'a>) -> Uchastok<'a> {
+    let mut uchastki = vec![];
+    let mut k: usize;
+    uchastki.push(X);
+    for i in 0..uchastki.len() {
+        if uchastki[i] == uch {
+            uchastki[i] = new_uchastok.clone();
+        }
+    }
+    new_uchastok
 }
